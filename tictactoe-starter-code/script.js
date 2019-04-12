82cd295a69f8a934d7a99e878f0b9ce48c157ded
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-04-12 10:03:21 +0100

    Made it impossible to click on a tile with something in

--- tictactoe-starter-code/script.js
+++ tictactoe-starter-code/script.js
@@ -6,6 +6,7 @@ function play() {
   var player = "O";
   $("td").each(function(index, div) {
   $(div).on("click", function () {
+    if (this.innerHTML == ("")) {
     this.innerHTML = turn
     this.className = turn
     $(".playerTurn")[0].innerHTML = "It is " + player + "'s turn";
@@ -16,6 +17,9 @@ function play() {
 
 
     result();
+    }
+
+    
     })
       })}
 
@@ -25,12 +29,12 @@ function result() {
   var tdArray = document.getElementsByTagName("td");
 
   if ((tdArray[0].innerHTML == "X" && tdArray[3].innerHTML == "X" && tdArray[6].innerHTML == "X" ) || (tdArray[1].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[7].innerHTML == "X" ) || (tdArray[2].innerHTML == "X" && tdArray[5].innerHTML == "X" && tdArray[8].innerHTML == "X" )
-  || (tdArray[0].innerHTML == "X" && tdArray[1].innerHTML == "X" && tdArray[2].innerHTML == "X" ) ||(tdArray[3].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[5].innerHTML == "X" )
+  || (tdArray[0].innerHTML == "X" && tdArray[1].innerHTML == "X" && tdArray[2].innerHTML == "X" ) ||(tdArray[3].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[5].innerHTML == "X" ) || (tdArray[6].innerHTML == "X" && tdArray[7].innerHTML == "X" && tdArray[8].innerHTML == "X" )
   || (tdArray[0].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[8].innerHTML == "X" ) ||(tdArray[2].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[6].innerHTML == "X" )) {
     $("td").html("X wins!") }
 
   else if ((tdArray[0].innerHTML == "O" && tdArray[3].innerHTML == "O" && tdArray[6].innerHTML == "O" ) || (tdArray[1].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[7].innerHTML == "O" ) || (tdArray[2].innerHTML == "O" && tdArray[5].innerHTML == "O" && tdArray[8].innerHTML == "O" )
-  || (tdArray[0].innerHTML == "O" && tdArray[1].innerHTML == "O" && tdArray[2].innerHTML == "O" ) ||(tdArray[3].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[5].innerHTML == "O" )
+  || (tdArray[0].innerHTML == "O" && tdArray[1].innerHTML == "O" && tdArray[2].innerHTML == "O" ) ||(tdArray[3].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[5].innerHTML == "O" ) || (tdArray[6].innerHTML == "X" && tdArray[7].innerHTML == "X" && tdArray[8].innerHTML == "X" )
   || (tdArray[0].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[8].innerHTML == "O" ) ||(tdArray[2].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[6].innerHTML == "O" )) {
     $("td").html("O wins!")}
 
