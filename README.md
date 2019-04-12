87e87c9827ca80a2b2d2c74c0fd6d34674b79236
Tech-A06 <tech-a06@tech-a06s-air.lan>
2019-04-12 07:43:56 +0100

    Finished the rest of the game including the results and the README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Sparta-js-tic-tac-toe
+
+
+## Task
+
+The task was to create a fully working tic-tac-toe game, including a readout of which player's go it was and a reset button. The game follows the traditional rules and the first player to get 3 of their symbols in a row wins. If neither player is able to manage this, the game is tied. When a player wins or there is a tie, the game is stopped and the result announced.
+
+## Instructions
+
+Clone and download the repo onto your local device and open the index.html in your web browser. Click on the boxes to start playing the game.
--- tictactoe-starter-code/script.js
+++ tictactoe-starter-code/script.js
@@ -1,73 +1,43 @@
 $(function(){
 
+function play() {
+  goes = 0;
   var turn = "X";
-  var player = "Y";
-
-
-
-
-
-// $("td").each(function(index, div) {
-//   $(div).on("click", function () {
-//     this.innerHTML = "X"
-//     this.addClass = "X"
-// })
-//   ;})
-
-
-
-function makeX() {
-$("td").each(function(index, div) {
+  var player = "O";
+  $("td").each(function(index, div) {
   $(div).on("click", function () {
     this.innerHTML = turn
     this.className = turn
     $(".playerTurn")[0].innerHTML = "It is " + player + "'s turn";
 
-
-    console.log(tdArray[0].innerHTML);
-    if ((tdArray[0].innerHTML == "X" && tdArray[3].innerHTML == "X" && tdArray[0].innerHTML == "X" ) || (tdArray[1].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[7].innerHTML == "X" ) || (tdArray[2].innerHTML == "X" && tdArray[5].innerHTML == "X" && tdArray[8].innerHTML == "X" )
-    || (tdArray[0].innerHTML == "X" && tdArray[1].innerHTML == "X" && tdArray[2].innerHTML == "X" ) ||(tdArray[3].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[5].innerHTML == "X" )
-    || (tdArray[0].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[8].innerHTML == "X" ) ||(tdArray[2].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[6].innerHTML == "X" )) {
-      $("td").html("X wins!")}
-
-
-    if ((tdArray[0].innerHTML == "O" && tdArray[3].innerHTML == "O" && tdArray[0].innerHTML == "O" ) || (tdArray[1].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[7].innerHTML == "O" ) || (tdArray[2].innerHTML == "O" && tdArray[5].innerHTML == "O" && tdArray[8].innerHTML == "O" )
-    || (tdArray[0].innerHTML == "O" && tdArray[1].innerHTML == "O" && tdArray[2].innerHTML == "O" ) ||(tdArray[3].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[5].innerHTML == "O" )
-    || (tdArray[0].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[8].innerHTML == "O" ) ||(tdArray[2].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[6].innerHTML == "O" )) {
-      $("td").html("O wins!")}
-
-
     turn == "X" ? turn = "O" : turn = "X"
+    goes ++
     player == "O" ? player = "X" : player = "O"
 
 
+    result();
+    })
+      })}
 
-})
-  ;})}
 
+
+function result() {
   var tdArray = document.getElementsByTagName("td");
 
+  if ((tdArray[0].innerHTML == "X" && tdArray[3].innerHTML == "X" && tdArray[6].innerHTML == "X" ) || (tdArray[1].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[7].innerHTML == "X" ) || (tdArray[2].innerHTML == "X" && tdArray[5].innerHTML == "X" && tdArray[8].innerHTML == "X" )
+  || (tdArray[0].innerHTML == "X" && tdArray[1].innerHTML == "X" && tdArray[2].innerHTML == "X" ) ||(tdArray[3].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[5].innerHTML == "X" )
+  || (tdArray[0].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[8].innerHTML == "X" ) ||(tdArray[2].innerHTML == "X" && tdArray[4].innerHTML == "X" && tdArray[6].innerHTML == "X" )) {
+    $("td").html("X wins!") }
 
-  // function makeY() {
-  // $("td").each(function(index, div) {
-  //   $(div).on("click", function () {
-  //     this.innerHTML = turn
-  //     this.className = turn
-  //     turn = "x"
-  //
-  // })
-  //   ;})}
+  else if ((tdArray[0].innerHTML == "O" && tdArray[3].innerHTML == "O" && tdArray[6].innerHTML == "O" ) || (tdArray[1].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[7].innerHTML == "O" ) || (tdArray[2].innerHTML == "O" && tdArray[5].innerHTML == "O" && tdArray[8].innerHTML == "O" )
+  || (tdArray[0].innerHTML == "O" && tdArray[1].innerHTML == "O" && tdArray[2].innerHTML == "O" ) ||(tdArray[3].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[5].innerHTML == "O" )
+  || (tdArray[0].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[8].innerHTML == "O" ) ||(tdArray[2].innerHTML == "O" && tdArray[4].innerHTML == "O" && tdArray[6].innerHTML == "O" )) {
+    $("td").html("O wins!")}
 
-function go () {
-  if (turn == "X") {
-    makeX()
-  }
-  else if (turn == "Y") {
-    makeY()
+  else if (goes == 9){
+    $("td").html("It's a tie!")
   }
 }
-
-
 
 
 
@@ -75,14 +45,12 @@ $("#reset").on("click", function () {
 
   $("td").html(" ")
   $("td").removeClass("X")
-  //remove class
-})
-// Set up a loop adding 3 and making an array-- if all array is the same there is a winner
-// if ($("td").attr("")) {
-//   alert("X wins")
-// }
+  goes = 0;
 
-go();
+})
+
+
+play();
 
 
 })
